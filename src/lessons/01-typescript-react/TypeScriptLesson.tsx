5eb8a5b972ccc203c9dda1db721c62aae4fa4252
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-07-19 10:44:02 +0300

    Replace inline variables with UserCard component and typed user object

--- src/lessons/01-typescript-react/TypeScriptLesson.tsx
+++ src/lessons/01-typescript-react/TypeScriptLesson.tsx
@@ -1,22 +1,18 @@
+import UserCard from './components/UserCard';
+
 function TypeScriptLesson() {
-  const name: string = 'Haitham';
-
-  const age: number = 20;
-
-  const isDeveloper: boolean = true;
+  const user = {
+    id: 1,
+    name: 'Haitham',
+    email: 'haithamabudraz@gmail.com',
+    isAdmin: true,
+  };
 
   return (
     <div>
       <h1>TypeScript with React</h1>
 
-      <p>Name: {name}</p>
-
-      <p>Age: {age}</p>
-
-      <p>
-        Developer:
-        {isDeveloper ? ' Yes' : ' No'}
-      </p>
+      <UserCard user={user} />
     </div>
   );
 }
