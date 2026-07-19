83df92e608f4206e1608253c31d09b552ca37b3e
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-07-19 10:49:10 +0300

    Replace UserCard with ProductList and typed product data

--- src/lessons/01-typescript-react/TypeScriptLesson.tsx
+++ src/lessons/01-typescript-react/TypeScriptLesson.tsx
@@ -1,18 +1,24 @@
-import UserCard from './components/UserCard';
+import ProductList from './components/ProductList';
 
 function TypeScriptLesson() {
-  const user = {
-    id: 1,
-    name: 'Haitham',
-    email: 'haithamabudraz@gmail.com',
-    isAdmin: true,
-  };
+  const products = [
+    {
+      id: 1,
+      title: 'Laptop',
+      price: 1200,
+    },
+    {
+      id: 2,
+      title: 'Mouse',
+      price: 40,
+    },
+  ];
 
   return (
     <div>
       <h1>TypeScript with React</h1>
 
-      <UserCard user={user} />
+      <ProductList products={products} />
     </div>
   );
 }
