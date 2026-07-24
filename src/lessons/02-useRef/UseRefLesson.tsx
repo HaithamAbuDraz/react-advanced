a680f3cbedfbbfcdc426631fde4dc3e872336829
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-07-24 11:58:02 +0300

    Add UseRefLesson demonstrating useRef with auto-focus input

---
+++ src/lessons/02-useRef/UseRefLesson.tsx
@@ -0,0 +1,12 @@
+import AutoFocusInput from './components/AutoFocusInput';
+
+function UseRefLesson() {
+  return (
+    <div>
+      <h1>useRef Hook – Lesson</h1>
+      <AutoFocusInput />
+    </div>
+  );
+}
+
+export default UseRefLesson;
