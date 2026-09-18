aa24d0d0f229eb07e6f27a6ae135931b00d01269
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-18 16:39:29 +0300

    Restore UseRefLesson to ScrollToTop

--- src/lessons/02-useRef/UseRefLesson.tsx
+++ src/lessons/02-useRef/UseRefLesson.tsx
@@ -1,10 +1,10 @@
-import NotesApp from './components/NotesApp';
+import ScrollToTop from './components/ScrollToTop';
 
 function UseRefLesson() {
   return (
     <div>
       <h1>useRef Hook – Lesson</h1>
-      <NotesApp />
+      <ScrollToTop />
     </div>
   );
 }
