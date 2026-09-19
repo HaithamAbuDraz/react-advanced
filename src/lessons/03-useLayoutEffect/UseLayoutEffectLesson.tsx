9531f092a5a397af72b0e63c2f0300f0a0ee7d9c
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-19 10:20:31 +0300

    Add UseLayoutEffectLesson component demonstrating useLayoutEffect

---
+++ src/lessons/03-useLayoutEffect/UseLayoutEffectLesson.tsx
@@ -0,0 +1,12 @@
+import ElementMeasurement from "./components/ElementMeasurement";
+
+function UseLayoutEffectLesson() {
+  return (
+    <div>
+      <h1>useLayoutEffect Hook – Lesson</h1>
+      <ElementMeasurement />
+    </div>
+  );
+}
+
+export default UseLayoutEffectLesson;
