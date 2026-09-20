cd99855bdbebec291ad37560804bad063c0a3824
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-20 16:53:06 +0300

    Add Task and TaskAction types for task reducer

---
+++ src/revision/React-Hooks/02-UseReducerLesson/types.ts
@@ -0,0 +1,10 @@
+export type Task = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+export type TaskAction =
+  | { type: 'added'; id: number; text: string }
+  | { type: 'changed'; task: Task }
+  | { type: 'deleted'; id: number };
