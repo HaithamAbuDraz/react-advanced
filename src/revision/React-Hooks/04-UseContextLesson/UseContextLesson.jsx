2627e314a0ebc4d33db2762c2c6150cc4f5036d1
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-22 16:27:26 +0300

    Add UseContextLesson component rendering ThemeDemo

---
+++ src/revision/React-Hooks/04-UseContextLesson/UseContextLesson.jsx
@@ -0,0 +1,10 @@
+import ThemeDemo from './examples/ThemeDemo';
+
+export default function UseContextLesson() {
+  return (
+    <div>
+      <h1>useContext Lesson</h1>
+      <ThemeDemo />
+    </div>
+  );
+}
